5b9e441cad7fecfceeeb7a1c19b7a7864293158e
Enno <enno@example.com>
2022-01-26 15:36:23 +0100

    add fadein + per color fade tables for koala

--- Makefile
+++ Makefile
@@ -17,6 +17,7 @@ FLAGSNG=-d -v -no-guess
 FLAGSNG2=-d -v -bitpair-colors 0,-1,-1,-1
 FLAGSFORCE=-d -v -bitpair-colors 0,11,12,15
 TESTPIC=testdata/ste_ghosts_goblins.gif
+#TESTPIC=testdata/veto_room_with_view.png
 
 png2prg: $(TARGET)
 
@@ -31,7 +32,7 @@ compress: $(TARGET).upx png2prg_darwin_amd64.upx png2prg_darwin_arm64.upx png2pr
 	$(UPX) $(UPXFLAGS) -o $@ $<
 	touch $@
 
-$(TARGET): $(SRC) $(DISPLAYERS)
+$(TARGET): $(SRC) $(DISPLAYERS) $(TESTPIC)
 	CGO_ENABLED=$(CGO) GOOS=linux GOARCH=amd64 go build $(GOBUILDFLAGS) -ldflags="$(LDFLAGS)" -o $@
 
 png2prg_darwin_amd64: $(SRC) $(DISPLAYERS)
@@ -49,14 +50,18 @@ test: $(TARGET)
 
 testpack: $(TARGET)
 	./$(TARGET) $(FLAGS) -o z.prg $(TESTPIC)
-	exomizer sfx basic -q -o zz_guess.prg z.prg
+	exomizer sfx basic -q -o zz_guess.sfx.exo z.prg
+	dali --sfx 2073 -o zz_guess.sfx.dali z.prg
 	./$(TARGET) $(FLAGSNG) -o z.prg $(TESTPIC)
-	exomizer sfx basic -q -o zz_noguess.prg z.prg
+	exomizer sfx basic -q -o zz_noguess.sfx.exo z.prg
+	dali --sfx 2073 -o zz_noguess.sfx.dali z.prg
 	./$(TARGET) $(FLAGSNG2) -o z.prg $(TESTPIC)
-	exomizer sfx basic -q -o zz_noguess2.prg z.prg
+	exomizer sfx basic -q -o zz_noguess2.sfx.exo z.prg
+	dali --sfx 2073 -o zz_noguess2.sfx.dali z.prg
 	./$(TARGET) $(FLAGSFORCE) -o z.prg $(TESTPIC)
-	exomizer sfx basic -q -o zz_force_manual_colors.prg z.prg
-	$(X64) zz_guess.prg >/dev/null
+	exomizer sfx basic -q -o zz_force_manual_colors.sfx.exo z.prg
+	dali --sfx 2073 -o zz_force_manual_colors.sfx.dali z.prg
+	$(X64) zz_guess.sfx.exo >/dev/null
 
 clean:
-	rm -f $(TARGET) png2prg_darwin_amd64 png2prg_darwin_arm64 png2prg_win_amd64.exe GEN_*.go *.prg *.upx
+	rm -f $(TARGET) png2prg_darwin_amd64 png2prg_darwin_arm64 png2prg_win_amd64.exe GEN_*.go *.prg *.exo *.dali *.upx
--- display_koala.asm
+++ display_koala.asm
@@ -1,9 +1,25 @@
 
+.const DEBUG = false
+.const GENDEBUG = false
+.const LOOP = false
 .const fade_speed = 1
+.const steps = 16
 .const bitmap     = $2000
 .const screenram  = $0400
 .const colorram   = $d800
 .const fade_pass_address = $4000
+.const src_screenram = $c000
+.const src_colorram = $c400
+
+.const zp_start = 10
+.const zp_screen_lo = zp_start + 0
+.const zp_screen_hi = zp_start + 1
+.const zp_d800_lo = zp_start + 2
+.const zp_d800_hi = zp_start + 3
+.const zp_src_screen_lo = zp_start + 4
+.const zp_src_screen_hi = zp_start + 5
+.const zp_src_d800_lo = zp_start + 6
+.const zp_src_d800_hi = zp_start + 7
 
 .import source "lib.asm"
 
@@ -16,41 +32,37 @@ basicend:
 .pc = $0819 "start"
 start:
 		sei
+		jsr generate_fade_pass
 		jsr vblank
 		ldx #0
 		stx $d011
+		stx $d020
+		stx $d021
 	!:
+		lda #0
+	.for (var i=0; i<4; i++) {
+		sta screenram+(i*$100),x
+		sta colorram+(i*$100),x
+	}
 	.for (var i=0; i<4; i++) {
 		lda koala_source+$1f40+(i*$100),x
-		sta screenram+(i*$100),x
+		sta src_screenram+(i*$100),x
 		lda koala_source+$2328+(i*$100),x
-		sta colorram+(i*$100),x
+		sta src_colorram+(i*$100),x
 	}
 		inx
 		bne !-
-		lda koala_source+$2710
-		sta $d021
-		lsr
-		lsr
-		lsr
-		lsr
-		sta $d020
-
 		lda #$ff
 !loop:
-smc_src:
-		ldx koala_source+$1f3f
-smc_dest:
-		stx bitmap+$1f3f
+smc_src:	ldx koala_source+$1f3f
+smc_dest:	stx bitmap+$1f3f
 		dcp smc_src+1
 		bne !+
 		dec smc_src+2
-	!:
-		dcp smc_dest+1
+	!:	dcp smc_dest+1
 		bne !+
 		dec smc_dest+2
-	!:
-		ldx smc_dest+2
+	!:	ldx smc_dest+2
 		cpx #>(bitmap-1)
 		bne !loop-
 
@@ -61,66 +73,90 @@ smc_dest:
 		jsr vblank
 		lda #$3b
 		sta $d011
-
-		jsr generate_t_color_fade
-		jsr generate_fade_pass
-
-		lda #$ef
-	!:	cmp $dc01
-		bne !-
-
-		ldy #10
+.pc = * "fade loop"
 !loop:
+smc_yval:	ldy #$0f
+		.if (DEBUG) inc $d020
+		jsr generate_phase_col_tables
+		.if (DEBUG) dec $d020
+
 		ldx #fade_speed
 	!:	jsr vblank
 		dex
 		bne !-
 
-		ldx $d021
+		ldx src_colorram+1000
 		lda t_color_fade,x
 		sta $d021
-		ldx $d020
+		lda src_colorram+1000
+		lsr
+		lsr
+		lsr
+		lsr
+		tax
 		lda t_color_fade,x
 		sta $d020
 
 		lda #$70
-	!:  cmp $d012
+	!:	cmp $d012
 		bne !-
+
+		.if (DEBUG) dec $d020
 		jsr fade_pass
+		.if (DEBUG) inc $d020
 
-		dey
+		dec smc_yval+1
+		lda smc_yval+1
+		cmp #$ff
+		beq !done+
+		cmp #7
 		bne !loop-
-		jmp $fce2
 
+		lda #$ef
+	!:	cmp $dc01
+		bne !-
+		beq !loop-
+!done:
+	.if (LOOP) {
+		lda #$ef
+	!:	cmp $dc01
+		bne !-
+		jsr reset_phase
+		lda #$0f
+		sta smc_yval + 1
+		jmp !loop-
+	} else {
+		jmp $fce2
+	}
 vblank:
-        :vblank()
-        rts
+		:vblank()
+		rts
 // --------------------------------
 .pc = * "generate_fade_pass"
-
-.const zp_start = $fb
-.const zp_screen_lo = zp_start + 0
-.const zp_screen_hi = zp_start + 1
-.const zp_d800_lo = zp_start + 2
-.const zp_d800_hi = zp_start + 3
 
 generate_fade_pass:
 		lda #<screenram
 		sta zp_screen_lo
 		sta zp_d800_lo
+		sta zp_src_screen_lo
+		sta zp_src_d800_lo
 		lda #>screenram
 		sta zp_screen_hi
 		lda #>colorram
 		sta zp_d800_hi
+		lda #>src_screenram
+		sta zp_src_screen_hi
+		lda #>src_colorram
+		sta zp_src_d800_hi
 
 		lax #$00
 		tay
 !loop:
-		lda #$af            // lax screen_lo
+		lda #$af            // lax zp_src_screen_lo
 		jsr store_byte
-		lda zp_screen_lo
+		lda zp_src_screen_lo
 		jsr store_byte
-		lda zp_screen_hi
+		lda zp_src_screen_hi
 		jsr store_byte
 		lda #$bd            // lda t_color_fade,x
 		jsr store_byte
@@ -135,11 +171,11 @@ generate_fade_pass:
 		lda zp_screen_hi
 		jsr store_byte
 
-		lda #$af            // lax d800_lo
+		lda #$af            // lax zp_src_d800_lo
 		jsr store_byte
-		lda zp_d800_lo
+		lda zp_src_d800_lo
 		jsr store_byte
-		lda zp_d800_hi
+		lda zp_src_d800_hi
 		jsr store_byte
 		lda #$bd            // lda t_color_fade,x
 		jsr store_byte
@@ -154,9 +190,13 @@ generate_fade_pass:
 		lda zp_d800_hi
 		jsr store_byte
 
+		inc zp_src_screen_lo
+		inc zp_src_d800_lo
 		inc zp_screen_lo
 		inc zp_d800_lo
 		bne !+
+		inc zp_src_screen_hi
+		inc zp_src_d800_hi
 		inc zp_screen_hi
 		inc zp_d800_hi
 	!:
@@ -177,58 +217,107 @@ store_byte:
 		bne !+
 		inc store_byte+2
 	!:	rts
-/*
-.C:4800  AF 00 04    LAX $0400
-.C:4803  BD 00 09    LDA $0900,X
-.C:4806  8D 00 04    STA $0400
-.C:4809  AF 00 D8    LAX $D800
-.C:480c  BD 00 09    LDA $0900,X
-.C:480f  8D 00 D8    STA $D800
-	.for (var i=0; i<1000; i++) {
-		lax $0400+i
-		lda t_color_fade,x
-		sta $0400+i
-		lax $d800+i
-		lda t_color_fade,x
-		sta $d800+i
-	}
-		rts
-*/
 // --------------------------------
-.pc = * "generate_t_total_colfade"
-// generate the full fade table, for both low nibble AND hi nibble colors
-// this is a fast approach, but really limits the fadecolors and order.
-generate_t_color_fade:
+.pc = * "generate_phase_col_tables"
+generate_phase_col_tables:
+!next_step:
 		lda #<t_color_fade
-		sta smc_cf + 1
-
-		ldx #0
+		sta smc_totpercol + 1
 !loop:
-		ldy #0
-	!:
-		lda t_fadecol,x
+		// start with color 0
+		ldx #0
+!:
+		// y points to hi-nibble of color x
+		ldy t_col2index,x // y = 0, $10, $20, .., $f0
+smc_fadepercol1:
+		lda t_fadepercol
 		asl
 		asl
 		asl
 		asl
-		ora t_fadecol,y
-smc_cf:
+smc_fadepercol2:
+		ora t_fadepercol,y
+smc_totpercol:
 		sta t_color_fade
-		inc smc_cf + 1
-		iny
-		cpy #$10
-		bne !-
-
+		.if (GENDEBUG) sta $d020
+		inc smc_totpercol + 1
 		inx
 		cpx #$10
-		bne !loop-
-		rts
+		bne !-
 
+		lda smc_fadepercol1 + 1
+		clc
+		adc #$10
+		sta smc_fadepercol1 + 1
+		bcc !loop-
+
+		inc smc_fadepercol1 + 1
+		inc smc_fadepercol2 + 1
+		rts
+// --------------------------------
+reset_phase:
+	.if (LOOP) {
+		lda #<t_fadepercol
+		sta smc_fadepercol1 + 1
+		sta smc_fadepercol2 + 1
+		rts
+	}
 // ------------------------------
-.pc = * "t_fadecol"
-t_fadecol:
-		.byte $00,$0d,$09,$0c,$02,$08,$00,$0f
-		.byte $02,$00,$08,$09,$04,$03,$04,$05
+.pc = * "fader.t_col2index"
+t_col2index:
+		.fill $10, i*$10
+// ------------------------------
+.align $100
+.pc = * "t_fadepercol"
+t_fadepercol:
+t_facecol_0:
+		.byte $0,$0,$0,$0, $0,$0,$0,$0
+		.byte $0,$0,$0,$0, $0,$0,$0,$0
+t_facecol_1:
+		.byte $9,$2,$8,$c, $a,$f,$7,$1
+		.byte $7,$f,$a,$c, $8,$2,$9,$0
+t_facecol_2:
+		.byte $0,$0,$0,$0, $0,$0,$9,$2
+		.byte $9,$0,$0,$0, $0,$0,$0,$0
+t_facecol_3:
+		.byte $0,$0,$6,$b, $4,$e,$5,$3
+		.byte $5,$e,$4,$b, $6,$0,$0,$0
+t_facecol_4:
+		.byte $0,$0,$0,$0, $0,$6,$b,$4
+		.byte $b,$6,$0,$0, $0,$0,$0,$0
+t_facecol_5:
+		.byte $0,$0,$0,$6, $b,$4,$e,$5
+		.byte $e,$4,$b,$6, $0,$0,$0,$0
+t_facecol_6:
+		.byte $0,$0,$0,$0, $0,$0,$0,$6
+		.byte $0,$0,$0,$0, $0,$0,$0,$0
+t_facecol_7:
+		.byte $0,$9,$2,$8, $c,$a,$f,$7
+		.byte $f,$a,$c,$8, $2,$9,$0,$0
+t_facecol_8:
+		.byte $0,$0,$0,$0, $0,$9,$2,$8
+		.byte $2,$9,$0,$0, $0,$0,$0,$0
+t_facecol_9:
+		.byte $0,$0,$0,$0, $0,$0,$0,$9
+		.byte $0,$0,$0,$0, $0,$0,$0,$0
+t_facecol_a:
+		.byte $0,$0,$0,$9, $2,$8,$c,$a
+		.byte $c,$8,$2,$9, $0,$0,$0,$0
+t_facecol_b:
+		.byte $0,$0,$0,$0, $0,$0,$6,$b
+		.byte $6,$0,$0,$0, $0,$0,$0,$0
+t_facecol_c:
+		.byte $0,$0,$0,$0, $9,$2,$8,$c
+		.byte $8,$2,$9,$0, $0,$0,$0,$0
+t_facecol_d:
+		.byte $0,$6,$b,$4, $e,$5,$3,$d
+		.byte $3,$5,$e,$4, $b,$6,$0,$0
+t_facecol_e:
+		.byte $0,$0,$0,$0, $6,$b,$4,$e
+		.byte $4,$b,$6,$0, $0,$0,$0,$0
+t_facecol_f:
+		.byte $0,$0,$9,$2, $8,$c,$a,$f
+		.byte $a,$c,$8,$2, $9,$0,$0,$0
 // ------------------------------
 .pc = * "koala_source" virtual
 koala_source:
